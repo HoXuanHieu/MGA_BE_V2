94627a3ece14dc96fbe38a66baea22f1dd3ec6db
HoXuanHieu <hxhieu123@gmail.com>
2024-07-08 21:32:55 +0700

    feat: add FileHelper

---
+++ Service/Helper/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Service.Helper
+{
+    public static class FileHelper
+    {
+        public static async Task<string> SaveImageAsync(string base64, string folderName, string fileName)
+        {
+            try
+            {
+                byte[] bytes = Convert.FromBase64String(base64);
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", folderName);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path = Path.Combine(path, fileName);
+                await File.WriteAllBytesAsync(path, bytes);
+                return path;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
