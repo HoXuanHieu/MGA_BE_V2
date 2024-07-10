52b3d5f05ddee0c200ee495b385c899899315fce
HoXuanHieu <hxhieu123@gmail.com>
2024-07-10 21:59:35 +0700

    feat: add chapter and manga controller

---
+++ Web_API/Controllers/Chapter/ChapterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers.Chapter
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChapterController : ControllerBase
+    {
+    }
+}
---
+++ Web_API/Controllers/Manga/MangaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Service;
+
+namespace Web_API.Controllers.Manga
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangaController : ControllerBase
+    {
+        private readonly IMangaService _service;
+        public MangaController(IMangaService service)
+        {
+            _service = service;
+        }
+
+        // add pagination
+        [HttpGet]
+        public async Task<List<MangaResponse>> getAllManga()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("/{mangaId}")]
+        public async Task<IActionResult> getMangaInfomationById(String mangaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateManga()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
