8c46f2f430a2d95b0f94ea2e5b0ca8a63b588387
HoXuanHieu <hxhieu123@gmail.com>
2024-07-13 23:14:04 +0700

    feat: add json helper

---
+++ Service/Helper/JsonHelper.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Service
+{
+    public static class JsonHelper
+    {
+        //method to serialize string to json T
+        public static T? Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        //method to serialize object to json string
+        public static string Serialize<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
