f2eb861e1dd6fdef2d01c5750c9f41e3551da464
HoXuanHieu <hxhieu123@gmail.com>
2024-07-14 17:32:39 +0700

    feat: add create manga service

--- Service/IService/Manga/IMangaService.cs
+++ Service/IService/Manga/IMangaService.cs
@@ -1,5 +1,10 @@
-﻿namespace Service;
+﻿using Models;
+using Web_API.ResponseModel;
+
+namespace Service;
 
 public interface IMangaService
 {
+    Task<ApiResponse<MangaResponse>> CreateMangaAsync(CreateMangaRequest request);
+
 }
