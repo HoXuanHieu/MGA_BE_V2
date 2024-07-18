cb16515d24fd187ab4ca871ef048f13cfbbce992
HoXuanHieu <hxhieu123@gmail.com>
2024-07-18 18:03:10 +0700

    fix: add missing file

--- Service/IService/Manga/IMangaService.cs
+++ Service/IService/Manga/IMangaService.cs
@@ -9,5 +9,6 @@ public interface IMangaService
     Task<ApiResponse<AllMangaResponse>> GetAllMangaAsync();
     Task<ApiResponse<List<MangaResponse>>> GetAllApproveAsync();
     Task<ApiResponse<List<MangaResponse>>> GetAllMangaByUserAsync(String userId);
-    Task<ApiResponse<MangaResponse>> GetMangaByIdAsync(String MangaId);
+    Task<ApiResponse<MangaResponse>> GetMangaByIdAsync(String mangaId);
+    Task<ApiResponse<Boolean>> DeleteMangaAsync(String mangaId);
 }
