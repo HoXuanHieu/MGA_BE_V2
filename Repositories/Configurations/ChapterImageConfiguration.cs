f343b514da84adf18d87d1ad2d944399be18df41
HoXuanHieu <hxhieu123@gmail.com>
2024-07-26 14:18:30 +0700

    feat: add chapter image configuration

---
+++ Repositories/Configurations/ChapterImageConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+using Models.Entities;
+
+namespace Repositories.Configurations
+{
+    public class ChapterImageConfiguration : IEntityTypeConfiguration<ChapterImangeEntity>
+    {
+        public void Configure(EntityTypeBuilder<ChapterImangeEntity> builder)
+        {
+            builder.HasKey(x => x.ChapterImageId);
+            builder.Property(x => x.LastActivity).HasDefaultValue("");
+            builder.Property(x => x.ChapterName).IsRequired().HasMaxLength(70);
+            builder.Property(x => x.DateCreate).IsRequired().HasDefaultValue(DateTime.Now);
+            builder.Property(x => x.ChapterImagePath).IsRequired().HasDefaultValue("");
+            builder.HasOne(x => x.Chapter).WithMany(x => x.ChapterImage).HasForeignKey(x => x.ChapterId);
+        }
+    }
+}
